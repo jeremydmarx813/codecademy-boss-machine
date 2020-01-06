6f867664997fc95cd158c3ab141f76c8bf6c06a6
jeremydmarx <jeremydmarx@gmail.com>
2020-01-06 17:06:52 -0600

    initial commit, get and post requests on minions done

---
+++ server/minions.js
@@ -0,0 +1,35 @@
+const minionsRouter = require('express').Router();
+
+module.exports = minionsRouter;
+
+const {
+    addToDatabase,
+    getAllFromDatabase,
+    getFromDatabaseById,
+    updateInstanceInDatabase,
+    deleteFromDatabasebyId
+} = require('./db');
+
+minionsRouter.param('minionId', (req, res, next, id) => {
+    const minion = getFromDatabaseById('minions', id);
+    if (minion) {
+      req.minion = minion;
+      next();
+    } else {
+      res.status(404).send();
+    }
+  });
+
+minionsRouter.get('/', (req, res, next) => {
+  res.send(getAllFromDatabase('minions'));
+});
+
+minionsRouter.post('/', (req, res, next) => {
+    const newMinion = addToDatabase('minions', req.body);
+    res.status(201).send(newMinion);
+ });
+
+minionsRouter.get('/:minionId', (req, res, next) => {
+    res.send(req.minion);
+});
+
